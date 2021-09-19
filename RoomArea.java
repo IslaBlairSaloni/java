afd70061d1b2bc87e7c8a6c0bfe9dcc4fd0498fa
IslaBlairSaloni <79751225+IslaBlairSaloni@users.noreply.github.com>
2021-09-19 20:37:34 +0530

    Create RoomArea.java

---
+++ RoomArea.java
@@ -0,0 +1,23 @@
+/* note that file name is the class name that
+ contains the main method */
+class Room
+{
+ float length;
+ float breadth;
+ void getData(float a, float b)
+ {
+   length=a;
+   length=b; 
+ }
+}
+class RoomArea //class for main method
+{
+ public static void main(String args[])
+ { 
+  float area;
+  Room R1=new Room(); //Room type object declaration
+  R1.getData(14.1,10.2); 
+  area=R1.length*R1.breadth;
+  System.out.println("area="+area);
+ }
+}
