4d424d91741dc8a47389d52f06467d47c6a7fe26
IslaBlairSaloni <79751225+IslaBlairSaloni@users.noreply.github.com>
2021-09-30 21:18:29 +0530

    Create Declaration.java

---
+++ Declaration.java
@@ -0,0 +1,14 @@
+public class Declaration
+{
+ public static void main(String args[])
+ {
+  byte a=45;
+  short b=4578;
+  int c=27370;
+  long d=737377L;
+  float e=23.4f;
+  double f=37.8; 
+  char g='h';
+  boolean h=true;
+ }
+}
