9c69a0d247d30a69ac03418134c4e64fba9a8a69
IslaBlairSaloni <79751225+IslaBlairSaloni@users.noreply.github.com>
2021-10-05 09:55:42 +0530

    Update Declaration.java

--- Declaration.java
+++ Declaration.java
@@ -1,11 +1,14 @@
 public class Declaration
 {
  public static void main(String args[])
- {
-  byte a=45;
-  short b=4578;
-  int c=27370;
-  long d=737377L;
+ { 
+  //declaration
+  int count; 
+  float x, y;
+  byte b;
+  //initialisation
+  double total= 75.36; 
+  char ch='x';
   float e=23.4f;
   double f=37.8; 
   char g='h';
