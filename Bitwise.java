086f172eed1c09bfcd93d70d0ac68bbc320d8344
Saloni <79751225+IslaBlairSaloni@users.noreply.github.com>
2021-10-11 03:32:04 +0530

    Create Bitwise.java

---
+++ Bitwise.java
@@ -0,0 +1,19 @@
+public class Bitwise  
+{   
+public static void main(String[] args)   
+{   
+  // bitwise OR
+  int x = 9, y = 8;    
+  // 1001 & 1000 = 1000 = 8  
+  System.out.println("x & y = " + (x & y));   
+  // bitwise XOR
+  int x = 9, y = 8;   
+  // 1001 ^ 1000 = 0001 = 1  
+  System.out.println("x ^ y = " + (x ^ y));
+  // bitwise OR
+  int x = 9, y = 8;   
+  // 1001 | 1000 = 1001 = 9  
+  System.out.println("x | y = " + (x | y));  
+    
+}  
+} 
